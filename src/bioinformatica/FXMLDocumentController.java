d2bd4379e2901c2495cfc871c1c3e6bf3384876b
Wesley Portella <wesley.portella@yahoo.com.br>
2018-04-03 22:01:01 -0300

    criado tela de questoes

--- src/bioinformatica/FXMLDocumentController.java
+++ src/bioinformatica/FXMLDocumentController.java
@@ -10,21 +10,50 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 /**
  *
  * @author usuario
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
+    private Button btnResponder;
+
+    @FXML
+    private RadioButton radioA;
+
+    @FXML
+    private RadioButton radioB;
+
+    @FXML
+    private RadioButton radioC;
+
+    @FXML
+    private RadioButton radioD;
+
+    @FXML
+    private Label labelRespostaA;
+
+    @FXML
+    private Label labelRespostaB;
+
+    @FXML
+    private Label labelRespostaC;
+
+    @FXML
+    private Label labelRespostaD;
+
+    @FXML
+    private Label labelQuestao;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        labelRespostaD.setText("Hello World!");
     }
     
     @Override
